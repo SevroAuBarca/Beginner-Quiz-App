71902540aa4735e1350ec755ed36cca40fc91b37
Daniel Contreras <daniel_contreras001@hotmail.com>
2021-12-23 21:01:31 -0600

    Eliminando opcion mateaticas, hay muy pocas preguntas como para añadirla

--- src/getQuestions.js
+++ src/getQuestions.js
@@ -1,8 +1,8 @@
-const getQuestions = async (amount, categorie, difficult) => {
+const getQuestions = async (amount, category, difficult) => {
   // const API = `"https://opentdb.com/api.php?amount=${amount}&category=9&difficulty=${difficult}&type=multiple"`;
 
   const data = await fetch(
-    `https://opentdb.com/api.php?amount=${amount}&category=${categorie}&difficulty=${difficult}&type=multiple`
+    `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficult}&type=multiple`
   );
   const { results } = await data.json();
   return results;
